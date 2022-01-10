8ba79890c4cb045aee37859dc49431e1f11d11b2
itsRunic <87670203+itsRunic@users.noreply.github.com>
2022-01-10 04:06:18 -0600

    Messed with positions and animations.

--- script.js
+++ script.js
@@ -1,8 +1,45 @@
 'use strict';
 
 //Selecting elements
-const startScreen = document.querySelector(`.start-screen`);
+const startScreenEl = document.querySelector(`.start-screen`);
 const noteMainEl = document.querySelector(`.note-app--main`);
 
-noteMainEl.classList.add(`hidden`);
-// startScreen.classList.add(`hidden`);
+noteMainEl.classList.add('hidden');
+
+// Start Screen Input
+document
+  .querySelector(`.start--input-name`)
+  .addEventListener('keydown', function (e) {
+    const userName = document.querySelector(`.start--input-name`).value;
+
+    const titleTransition = () => {
+      document
+        .querySelector(`.start--title`)
+        .classList.add(`transition--title`);
+      document
+        .querySelector(`.start--input-name`)
+        .classList.add(`transition--fade-out`);
+      document
+        .querySelector(`.seperator`)
+        .classList.add(`transition--fade-out`);
+      document.querySelector(`.start--instruction`).textContent =
+        'Welcome ' + userName;
+      document
+        .querySelector(`.start--instruction`)
+        .classList.add(`transition--fade-out`);
+    };
+
+    const waitForTransition = () => {
+      titleTransition();
+
+      setTimeout(() => {
+        startScreenEl.classList.add(`hidden`);
+        noteMainEl.classList.remove(`hidden`);
+      }, 1500);
+    };
+
+    if (e.key === 'Enter' && noteMainEl.classList.contains(`hidden`)) {
+      waitForTransition();
+      noteMainEl.classList.add(`transition--fade`);
+    }
+  });
